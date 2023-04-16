8550cd5f1ef6521010dc5a3441242209e32769e2
John Duprey <jwd@johnwduprey.com>
2023-04-16 14:36:22 -0400

    PSScriptAnalyzer Fixes

--- DuoSecurity/Public/Misc/Get-DuoAccountID.ps1
+++ DuoSecurity/Public/Misc/Get-DuoAccountID.ps1
@@ -19,24 +19,25 @@ function Get-DuoAccountID {
         [Alias('api_hostname')]
         [string]$ApiHost
     )
-    $AccountHex = ''
 
-    if (!$ApiHost) {
-        if ($script:DuoApiHost) { $ApiHost = $script:DuoApiHost }
-        elseif ($script:DuoAccountsApiHost) { $ApiHost = $script:DuoAccountsApiHost }
-    }
+    process {
+        $AccountHex = ''
 
-    if (!$ApiHost) {
-        $ApiHost = Read-Host 'Please enter an API hostname (e.g. api-01ab23cd.duosecurity.com)'
-    }
+        if (!$ApiHost) {
+            if ($script:DuoApiHost) { $ApiHost = $script:DuoApiHost }
+            elseif ($script:DuoAccountsApiHost) { $ApiHost = $script:DuoAccountsApiHost }
+        }
 
-    if ($ApiHost -match 'api-(?<AccountHex>.+)\.duosecurity\.com') {
-        $AccountHex = '0x{0}' -f $Matches.AccountHex
-        $AccountHexUnsigned = [uint]$AccountHex
+        if (!$ApiHost) {
+            $ApiHost = Read-Host 'Please enter an API hostname (e.g. api-01ab23cd.duosecurity.com)'
+        }
+
+        if ($ApiHost -match 'api-(?<AccountHex>.+)\.duosecurity\.com') {
+            $AccountHex = '0x{0}' -f $Matches.AccountHex
+            $AccountHexUnsigned = [uint]$AccountHex
         ($AccountHexUnsigned.ToString().PadLeft(10, '0') -split '([0-9]{4})' -ne '') -join '-'
-    }
-
-    else {
-        Write-Output 'Unable to determine account ID'
+        } else {
+            throw 'Unable to determine account ID'
+        }
     }
 }
